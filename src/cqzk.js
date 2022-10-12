9c06faa8d3a0f280c7ff81b4ed8385038a5a72ab
香辣脆小浣熊 <xianglacuixiaohuanxiong@gmail.com>
2022-10-12 16:47:23 +0800

    fix: 被墙了国外访问

--- src/cqzk.js
+++ src/cqzk.js
@@ -9,36 +9,37 @@ if (!CQZK) {
   login()
 }
 
-function login () {
-  console.log(`正常运行`);
+async function login () {
   service.getCodeService().then((base64) => {
     console.log(base64);
-    // const token = 'Basic YWRtaW46MTc0NDQzNzMzMw==';
-    // service.checkCodeService(base64.data, token).then((code) => {
-    //   const params = {
-    //     sfzh: CQZK.split('&&')[0],
-    //     password: CQZK.split('&&')[1],
-    //     captcha: code,
-    //     captchacode: base64.id,
-    //     xmid: '0838d736f15e469da6ade16ad64755bb'
-    //   }
-    //   service.loginService(params).then((token) => {
-    //     service.healthReport(token.token).then(() => {
-    //       console.log(`上报完成`)
-    //     }).catch(err => {
-    //       console.log(`上报失败:`, err);
-    //       sendEmail(false, [{ msg: '上报失败,错误原因请前往github查看' }]);
-    //     })
-    //   }).catch(err => {
-    //     if (err.status === 202) {
-    //       return login()
-    //     }
-    //     console.log('出错:', err)
-    //   })
-    // }).catch(err => {
-    //   console.log(err)
-    // })
+    const token = 'Basic YWRtaW46MTc0NDQzNzMzMw==';
+    service.checkCodeService(base64.data, token).then((code) => {
+      const params = {
+        sfzh: CQZK.split('&&')[0],
+        password: CQZK.split('&&')[1],
+        captcha: code,
+        captchacode: base64.id,
+        xmid: '0838d736f15e469da6ade16ad64755bb'
+      }
+      service.loginService(params).then((token) => {
+        service.healthReport(token.token).then(() => {
+          console.log(`上报完成`)
+        }).catch(err => {
+          console.log(`上报失败:`, err);
+          sendEmail(false, [{ msg: '上报失败,错误原因请前往github查看' }]);
+        })
+      }).catch(err => {
+        if (err.status === 202) {
+          return login()
+        }
+        console.log('出错:', err)
+      })
+    }).catch(err => {
+      console.log(err)
+      sendEmail(false, [{ msg: '上报失败,错误原因请前往github查看' }]);
+    })
   }).catch(err => {
-    console.log(`请求失败`, err)
+    console.log(`请求失败`, err);
+    sendEmail(false, [{ msg: '上报失败,错误原因请前往github查看' }]);
   })
 }
